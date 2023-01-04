d30252a1bd88b6cd2df8b4bae94fd8a82ed6ac67
oleg.shilo <oleg.shilo@gmail.com>
2023-01-05 00:44:02 +1100

    - Clean up

--- PyMap/ToolWindow1Control.xaml.cs
+++ PyMap/ToolWindow1Control.xaml.cs
@@ -377,31 +377,4 @@ namespace PyMap
             catch { }
         }
     }
-
-    internal class IDEEventsListener : IVsRunningDocTableEvents3
-    {
-        public int OnBeforeSave(uint docCookie)
-        {
-            ThreadHelper.ThrowIfNotOnUIThread();
-
-            return VSConstants.S_OK;
-        }
-
-        public int OnAfterFirstDocumentLock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining) => VSConstants.S_OK;
-
-        public int OnBeforeLastDocumentUnlock(uint docCookie, uint dwRDTLockType, uint dwReadLocksRemaining, uint dwEditLocksRemaining) => VSConstants.S_OK;
-
-        public int OnAfterSave(uint docCookie) => VSConstants.S_OK;
-
-        public int OnAfterAttributeChange(uint docCookie, uint grfAttribs) => VSConstants.S_OK;
-
-        public int OnBeforeDocumentWindowShow(uint docCookie, int fFirstShow, IVsWindowFrame pFrame) => VSConstants.S_OK;
-
-        public int OnAfterDocumentWindowHide(uint docCookie, IVsWindowFrame pFrame) => VSConstants.S_OK;
-
-        public int OnAfterAttributeChangeEx(uint docCookie, uint grfAttribs, IVsHierarchy pHierOld, uint itemidOld, string pszMkDocumentOld, IVsHierarchy pHierNew, uint itemidNew, string pszMkDocumentNew) => VSConstants.S_OK;
-
-        public void OnAfterDocumentLockCountChanged(uint docCookie, uint dwRDTLockType, uint dwOldLockCount, uint dwNewLockCount)
-        { }
-    }
 }
